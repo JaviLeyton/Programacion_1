cccbfc0855599e519cf4c7b3c38dec7ad1c6ddc0
JaviLeyton <oscarj.leytono@uqvirtual.edu.co>
2023-08-22 20:08:57 -0500

    :+1:  creacion atributos

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Libro.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/model/Libro.java
@@ -8,29 +8,41 @@ public class Libro {
     public String editorial;
     public Integer pageNumber;
 
-    // MÉTODOS
-
-    public String getTitle(){
-        return this.title;
-    }
-
+    // MÉTODOS PARA OBTENER INFORMACIÓN
+    public String getTitle(){ return this.title; }
     public String getGender(){
         return this.gender;
     }
-
     public String getAuthor(){
         return this.author;
     }
     public Integer getPublicationYear(){
         return this.publicationYear;
     }
-
     public String getEditorial(){
         return this.editorial;
     }
-
     public Integer getPageNumber(){
         return this.pageNumber;
     }
-    
+
+    // Métodos para editar información
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public void setGender(String gender){
+        this.gender=gender;
+    }
+    public void setAuthor(String author){
+        this.author=author;
+    }
+    public void setPublicationYear(Integer publicationYear){
+        this.publicationYear=publicationYear;
+    }
+    public void setEditorial(String editorial){
+        this.editorial=editorial;
+    }
+    public void setPageNumber(Integer pageNumber){
+        this.pageNumber=pageNumber;
+    }
 }
